a6fe3bd80d700a5ee00dc846463af937d4360f49
ProgrammerFront077 <alirezakh0090@gmail.com>
2023-08-25 12:15:48 +0430

    (Add):<-Ui(Spinner,Error) , Data On HomePage Status->

--- src/Hook/useGetProduct/index.tsx
+++ src/Hook/useGetProduct/index.tsx
@@ -2,10 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import { asyncGetProduct } from '../../StateManagment/Service/Product';
 
 const useGetProduct = () => {
-  const { data, status } = useQuery({
+  const { data, status, error } = useQuery({
     queryKey: ['product'],
     queryFn: asyncGetProduct,
   });
+
   return { data, status };
 };
 
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,4 +1,12 @@
+import Error from '../../Components/Ui/Error';
+import Spinner from '../../Components/Ui/Spinner';
+import useGetProduct from '../../Hook/useGetProduct';
+
 const HomePage = () => {
+  const { data, status } = useGetProduct();
+  if (status === 'loading') return <Spinner />;
+  if (status === 'error') return <Error />;
+  console.log(data);
   return <div className="flex space-x-4">home page</div>;
 };
 
